4187148c9e7c0197a2172eaccced8b34eb1dd08b
Kevin Giovanni <kevin.giovanni@ti.ukdw.ac.id>
2019-07-02 19:45:00 +0700

    add csrf token and favicon

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,8 +2,12 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="Cache-control" content="public">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="icon" type="image/png" href="{{ asset('app-asset/images/icons/favicon.png') }}"/>
     <title>@yield('title')</title>
 
     <link rel="stylesheet" type="text/css" href="{{ URL::asset('css/app.css') }}">
