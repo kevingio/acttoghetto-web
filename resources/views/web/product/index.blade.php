35c8c99e02291fabc978daeea605753c6fdfdee5
Albertus Istora Pahala <albertus.istora@ti.ukdw.ac.id>
2019-07-05 10:13:25 +0700

    add check box category

--- resources/views/web/product/index.blade.php
+++ resources/views/web/product/index.blade.php
@@ -31,41 +31,60 @@
                         </div>
 
                         <!--  -->
-                        <h4 class="m-text14 p-b-7">
-                            Categories
-                        </h4>
+                        <form action="" method="">                     
+                            <ul class="p-b-54">
+                                <li class="p-t-4">
+                                    <h4 class="m-text14 p-b-7">
+                                        Gender
+                                    </h4>  
+                                </li>
 
-                        <ul class="p-b-54">
-                            <li class="p-t-4">
-                                <a href="#" class="s-text13 active1">
-                                    All
-                                </a>
-                            </li>
+                                <li class="p-t-4">
+                                    <div class="custom-control checkbox-style mb-1">
+                                        <input type="checkbox" class="custom-control-input" id="checkBox1" name="">
+                                        <label class="custom-control-label" for="checkBox1">All</label>
+                                    </div>
+                                </li>
 
-                            <li class="p-t-4">
-                                <a href="#" class="s-text13">
-                                    Women
-                                </a>
-                            </li>
+                                <li class="p-t-4">
+                                    <div class="custom-control checkbox-style mb-1">
+                                        <input type="checkbox" class="custom-control-input" id="checkBox2" name="">
+                                        <label class="custom-control-label" for="checkBox2">Man</label>
+                                    </div>
+                                </li>
 
-                            <li class="p-t-4">
-                                <a href="#" class="s-text13">
-                                    Men
-                                </a>
-                            </li>
+                                <li class="p-t-4">
+                                    <div class="custom-control checkbox-style">
+                                        <input type="checkbox" class="custom-control-input" id="checkBox3" name="">
+                                        <label class="custom-control-label" for="checkBox3">Woman</label>
+                                    </div>
+                                </li>
 
-                            <li class="p-t-4">
-                                <a href="#" class="s-text13">
-                                    Kids
-                                </a>
-                            </li>
+                                <li class="p-t-4">
+                                    <hr class="mb-3">
+                                </li>
 
-                            <li class="p-t-4">
-                                <a href="#" class="s-text13">
-                                    Accesories
-                                </a>
-                            </li>
-                        </ul>
+                                <li class="p-t-4">
+                                    <h4 class="m-text14 p-b-7">
+                                        Categories
+                                    </h4>  
+                                </li>
+
+                                <li class="p-t-4">
+                                    <div class="custom-control checkbox-style mb-1">
+                                        <input type="checkbox" class="custom-control-input" id="checkBox4" name="">
+                                        <label class="custom-control-label" for="checkBox4">All</label>
+                                    </div>
+                                </li>
+
+                                <li class="p-t-4">
+                                    <div class="custom-control checkbox-style mb-1">
+                                        <input type="checkbox" class="custom-control-input" id="checkBox5" name="">
+                                        <label class="custom-control-label" for="checkBox5">Shoes</label>
+                                    </div>
+                                </li>
+                            </ul>
+                        </form>
                     </div>
                 </div>
 
