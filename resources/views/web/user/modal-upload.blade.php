ff66ea13b1e6d12eca2c4fcf00184ee1a87c0e03
Kevin Giovanni <kevin.giovanni@ti.ukdw.ac.id>
2019-07-06 11:21:17 +0700

    server side datatable my transaction

--- resources/views/web/user/modal-upload.blade.php
+++ resources/views/web/user/modal-upload.blade.php
@@ -5,13 +5,13 @@
         <!-- Modal content-->
         <div class="modal-content">
         <div class="modal-header">
-            <h4 class="modal-title">Upload Bukti Pembayaran</h4>
+            <h5 class="modal-title">Upload Bukti Pembayaran</h5>
             <button type="button" class="close" data-dismiss="modal">&times;</button>
         </div>
         <div class="modal-body">
             <input type='file' id="imageUpload" accept=".png, .jpg, .jpeg" />
             <label for="imageUpload"></label>
-            <img id="previewImage" src="http://placehold.it/180" alt="your image" />
+            <img id="previewImage" class="pt-3" src="http://placehold.it/180" alt="your image" />
         </div>
         <div class="modal-footer">
             <button type="button" class="btn btn-success" data-dismiss="modal">Simpan</button>
