012efbc972c96c41b42ae859478c5869e955ae6e
Kevin Giovanni <kevin.giovanni@ti.ukdw.ac.id>
2019-07-11 20:10:47 +0700

    change all favicon

--- resources/views/web/landing.blade.php
+++ resources/views/web/landing.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/png" href="{{ asset('assets/images/icons/favicon.png') }}"/>
+    <link rel="icon" type="image/png" href="{{ asset('favicon.ico') }}"/>
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Acttoghetto</title>
     <style>
