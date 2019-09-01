4ec628a9a4e6cca0c44dc77dce28a0386a2810b0
Albertus Istora Pahala <albertus.istora@ti.ukdw.ac.id>
2019-09-01 22:11:47 +0700

    Add Tribute on footer

--- resources/views/layouts/parts/footer.blade.php
+++ resources/views/layouts/parts/footer.blade.php
@@ -1,7 +1,15 @@
 <footer class="bg-black p-t-45 p-b-43 p-l-45 p-r-45">
     <div class="t-center p-l-15 p-r-15">
         <div class="text-white t-center s-text8 p-t-20">
-            2019. ActtoGhetto</a>
+            <p>2019. ActtoGhetto</p>
+            <p> Made with 
+                <i class="fas fa-heart"></i> 
+                by 
+                <a href="http://www.kevingiovanni.com">Kevin Giovanni</a>
+                 & 
+            <a href="http://www.albertusip.com">Albertus IP</a>
+            </p>
+            
         </div>
     </div>
 </footer>
