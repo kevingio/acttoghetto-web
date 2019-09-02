79fb93d460e1918856526d12e62f22f7552c7d40
Kevin Giovanni <kevin.giovanni@tokopedia.com>
2019-09-02 09:40:47 +0700

    apply dark theme in admin page

--- resources/views/admin/layouts/parts/sidebar.blade.php
+++ resources/views/admin/layouts/parts/sidebar.blade.php
@@ -1,5 +1,5 @@
 <!-- ========== Left Sidebar Start ========== -->
-<div class="left side-menu">
+<div class="left side-menu bg-dark">
     <div class="slimscroll-menu" id="remove-scroll">
 
         <!--- Sidemenu -->
