011f47002dc1f1b60a7d50df88283400ae6bd38c
Kevin Giovanni <kevinevraldo@gmail.com>
2019-09-11 23:04:09 +0700

    redirect landing to admin if user already logged in

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -41,6 +41,9 @@ class HomeController extends Controller
      */
     public function landing()
     {
+        if(auth()->check()) {
+            return redirect('/admin/transaction');
+        }
         return view('web.landing');
     }
 
